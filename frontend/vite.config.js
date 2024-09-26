799342ac34b84b1795948fb09db97661fdeec284
Martin <bumko96@gmail.com>
2024-09-26 13:45:17 +0200

    finished dockerizing

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true, // needed for the Docker Container port mapping to work
-    strictPort: true,
-    port: 5173, // you can replace this port with any port
-  },
-  build: {
-    outDir: 'dist',
-  },
-})
+ base: "/",
+ plugins: [react()],
+ preview: {
+  port: 5173,
+  strictPort: true,
+ },
+ server: {
+  port: 5173,
+  strictPort: true,
+  host: true,
+ },
+});
