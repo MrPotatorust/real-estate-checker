b15abf282f57a0384cb9a0c9ff4213d0028301e9
Martin <bumko96@gmail.com>
2024-09-27 11:20:43 +0200

    Started doing the models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.postgres.fields import ArrayField
 # Create your models here.
+
+
+class Person(models.Model):
+    city = models.CharField(max_length=50)
+    title = models.CharField(max_length=100)
+    link = models.CharField(max_length=200)
+    imgs = ArrayField(models.CharField(max_length=100), blank=True) 
