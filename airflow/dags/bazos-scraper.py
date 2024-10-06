63cbc7ccba5641adc45d61022b25df4abb845f37
MrPotatorust <bumko96@gmail.com>
2024-10-06 12:10:48 +0200

    Added bazos scraper

--- airflow/dags/bazos-scraper.py
+++ airflow/dags/bazos-scraper.py
@@ -16,12 +16,13 @@ logger = logging.getLogger(__name__)
 
 
 def getHtmlDoc(url):
-
     response = requests.get(url)
 
 
     return response.text
 
+
+slovak_cities = []
 
 
 html_doc = getHtmlDoc("https://reality.bazos.sk/")
@@ -30,9 +31,19 @@ counter = 0
 
 
 while not soup.find(string="Stránka nenájdená"):
+    elements = soup.find_all(class_="inzeratyflex")
 
-
+    for element in elements:
+        element_loc = element.find(class_="inzeratylok").text
+        first_num = re.search(f'\d', element_loc)
+        if first_num:
+            if element_loc[:first_num.start()] not in slovak_cities:
+                slovak_cities.append(element_loc[:first_num.start()])
 
     counter += 20
     html_doc = getHtmlDoc(f"https://reality.bazos.sk/{counter}/")
     soup = BeautifulSoup(html_doc, "lxml")
+    print(f"scraped page {counter}")
+
+
+print(slovak_cities)
