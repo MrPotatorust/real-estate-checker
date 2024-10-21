91d7372585c03e6a433d0ab521c5846ab48df1a8
MrPotatorust <bumko96@gmail.com>
2024-10-21 21:53:46 +0200

    reworking

--- airflow/dags/nehnutelnosti-scraper.py
+++ airflow/dags/nehnutelnosti-scraper.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
-from airflow import DAG
-from airflow.operators.python import PythonOperator
+# from airflow import DAG
+# from airflow.operators.python import PythonOperator
 
 from bs4 import BeautifulSoup, NavigableString
 import requests
@@ -15,22 +15,22 @@ import logging
 
 
 
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2023, 1, 1),
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-}
+# default_args = {
+#     'owner': 'airflow',
+#     'depends_on_past': False,
+#     'start_date': datetime(2023, 1, 1),
+#     'email_on_failure': False,
+#     'email_on_retry': False,
+#     'retries': 1,
+#     'retry_delay': timedelta(minutes=5),
+# }
 
-my_dag = DAG(
-    dag_id = "Nehnutelnosti_scraper",
-    default_args=default_args,
-    description="Scrapes and converts to dataframe",
-    schedule_interval=timedelta(days=1),
-)
+# my_dag = DAG(
+#     dag_id = "Nehnutelnosti_scraper",
+#     default_args=default_args,
+#     description="Scrapes and converts to dataframe",
+#     schedule_interval=timedelta(days=1),
+# )
 
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
@@ -40,7 +40,7 @@ logger = logging.getLogger(__name__)
 def scraping():
 
 
-    slovak_cities=['bratislava', 'kosice', 'presov', 'zilina', 'nitra', 'banska-bystrica', 'trnava', 'trencin', 'martin', 'poprad', 'prievidza', 'zvolen', 'povazska-bystrica', 'nove-zamky', 'michalovce', 'spisska-nova-ves', 'komarno', 'levice', 'liptovsky-mikulas', 'humenne', 'bardejov', 'piestany', 'ruzomberok', 'lucenec', 'pezinok', 'topolcany', 'dunajska-streda', 'trebisov', 'cadca', 'dubnica-nad-vahom', 'rimavska-sobota', 'partizanske', 'vranov-nad-toplou', 'sala', 'senec', 'brezno', 'hlohovec', 'nove-mesto-nad-vahom', 'senica', 'malacky', 'snina', 'dolny-kubin', 'roznava', 'puchov', 'ziar-nad-hronom', 'banovce-nad-bebravou', 'stara-lubovna', 'handlova', 'skalica', 'galanta', 'kezmarok', 'sered', 'kysucke-nove-mesto', 'levoca', 'samorin', 'detva', 'stupava', 'sabinov', 'zlate-moravce', 'bytca', 'revuca', 'holic', 'myjava', 'velky-krtis', 'kolarovo', 'nova-dubnica', 'moldava-nad-bodvou', 'stropkov', 'svidnik', 'filakovo', 'sturovo', 'banska-stiavnica', 'surany', 'modra', 'tvrdosin', 'krompachy', 'secovce', 'velke-kapusany', 'stara-tura', 'vrable', 'velky-meder', 'svit', 'krupina', 'namestovo', 'vrutky', 'kralovsky-chlmec', 'hurbanovo', 'hrinova', 'liptovsky-hradok', 'sahy', 'trstena', 'turzovka', 'velky-saris', 'nova-bana', 'tornala', 'spisska-bela', 'zeliezovce', 'krasno-nad-kysucou', 'hnusta', 'lipany', 'nemsova', 'turcianske-teplice', 'svaty-jur', 'sobrance', 'gelnica', 'rajec', 'medzilaborce', 'zarnovica', 'vrbove', 'ilava', 'sladkovicovo', 'gabcikovo', 'poltar', 'dobsina', 'bojnice', 'nesvady', 'sastin-straze', 'gbely', 'sliac', 'kremnica', 'brezova-pod-bradlom', 'strazske', 'novaky', 'medzev', 'turany', 'giraltovce', 'trencianske-teplice', 'leopoldov', 'vysoke-tatry', 'spisske-podhradie', 'hanusovce-nad-toplou', 'tisovec', 'tlmace', 'cierna-nad-tisou', 'spisske-vlachy', 'jelsava', 'podolinec', 'rajecke-teplice', 'spisska-stara-ves', 'modry-kamen', 'dudince']
+    # slovak_cities=['bratislava', 'kosice', 'presov', 'zilina', 'nitra', 'banska-bystrica', 'trnava', 'trencin', 'martin', 'poprad', 'prievidza', 'zvolen', 'povazska-bystrica', 'nove-zamky', 'michalovce', 'spisska-nova-ves', 'komarno', 'levice', 'liptovsky-mikulas', 'humenne', 'bardejov', 'piestany', 'ruzomberok', 'lucenec', 'pezinok', 'topolcany', 'dunajska-streda', 'trebisov', 'cadca', 'dubnica-nad-vahom', 'rimavska-sobota', 'partizanske', 'vranov-nad-toplou', 'sala', 'senec', 'brezno', 'hlohovec', 'nove-mesto-nad-vahom', 'senica', 'malacky', 'snina', 'dolny-kubin', 'roznava', 'puchov', 'ziar-nad-hronom', 'banovce-nad-bebravou', 'stara-lubovna', 'handlova', 'skalica', 'galanta', 'kezmarok', 'sered', 'kysucke-nove-mesto', 'levoca', 'samorin', 'detva', 'stupava', 'sabinov', 'zlate-moravce', 'bytca', 'revuca', 'holic', 'myjava', 'velky-krtis', 'kolarovo', 'nova-dubnica', 'moldava-nad-bodvou', 'stropkov', 'svidnik', 'filakovo', 'sturovo', 'banska-stiavnica', 'surany', 'modra', 'tvrdosin', 'krompachy', 'secovce', 'velke-kapusany', 'stara-tura', 'vrable', 'velky-meder', 'svit', 'krupina', 'namestovo', 'vrutky', 'kralovsky-chlmec', 'hurbanovo', 'hrinova', 'liptovsky-hradok', 'sahy', 'trstena', 'turzovka', 'velky-saris', 'nova-bana', 'tornala', 'spisska-bela', 'zeliezovce', 'krasno-nad-kysucou', 'hnusta', 'lipany', 'nemsova', 'turcianske-teplice', 'svaty-jur', 'sobrance', 'gelnica', 'rajec', 'medzilaborce', 'zarnovica', 'vrbove', 'ilava', 'sladkovicovo', 'gabcikovo', 'poltar', 'dobsina', 'bojnice', 'nesvady', 'sastin-straze', 'gbely', 'sliac', 'kremnica', 'brezova-pod-bradlom', 'strazske', 'novaky', 'medzev', 'turany', 'giraltovce', 'trencianske-teplice', 'leopoldov', 'vysoke-tatry', 'spisske-podhradie', 'hanusovce-nad-toplou', 'tisovec', 'tlmace', 'cierna-nad-tisou', 'spisske-vlachy', 'jelsava', 'podolinec', 'rajecke-teplice', 'spisska-stara-ves', 'modry-kamen', 'dudince']
 
 
 
@@ -60,137 +60,93 @@ def scraping():
         "location": [],
         "location_description": [],
         "buy": [],
-        "house": []
     }
 
 
         
-    counter = 1
 
 
     logger.info("Started scraping nehnutelnosti.sk")
 
-    sale_rent_text = ""
-    house_apartment_text = ""
-
-    # 1 = houses for sale
-    # 2 = apartments for sale
-    # 3 = houses for rent
-    # 4 = apartments for rent
 
     main_start = time.time()
 
-    for sale_rent_bool in [1, 2, 3, 4]:
-        if sale_rent_bool == 1:
-            house_apartment_text = "domy"
-            sale_rent_text = "predaj"
-        elif sale_rent_bool == 2:
-            house_apartment_text = "byty"
-            sale_rent_text = "predaj"
-        elif sale_rent_bool == 3:
-            house_apartment_text = "domy"
-            sale_rent_text = "prenajom"
-        else:
-            house_apartment_text = "byty"
-            sale_rent_text = "prenajom"
+    page_counter = 0
         
 
-        for cities in enumerate(slovak_cities):
+    while not page_counter or not soup.find(string="Nenašli sa žiadne výsledky"):
+        page_counter +=1
 
-                
+        url = f"https://www.nehnutelnosti.sk/vyhladavanie/?p[page]={page_counter}"
+        logger.info(url)
+        logger.info(f"Started scraping page {page_counter}")
 
-            url = f"https://www.nehnutelnosti.sk/{cities[1]}/{house_apartment_text}/{sale_rent_text}/?p[page]={counter}"
-            logger.info(url)
-            logger.info(f"Started scraping {cities[0]+1}, {cities[1]}")
+        html_doc = getHtmlDoc(url)
+        soup = BeautifulSoup(html_doc, 'lxml')
 
-            html_doc = getHtmlDoc(url)
-            soup = BeautifulSoup(html_doc, 'lxml')
+        start = time.time()
 
-            start = time.time()
+        
+        for element in soup.find_all('div', {"class": "advertisement-item"}):
+            sq_m = None
 
-            page_counter=0
+            img = element.find_all(class_="position-relative")[1:2][0].find("data-img").get("data-src")
 
-            while not soup.find(string="Nenašli sa žiadne výsledky"):
+            description = element.find(class_="truncate-text").text.strip().replace("\n", "").replace("\r", "")
 
-                page_counter +=1
-
-                logger.info(f"Started scraping next page {page_counter}")
-                
-                for element in soup.find_all('div', {"class": "advertisement-item"}):
-                    sq_m = None
-
-                    img = element.find_all(class_="position-relative")[1:2][0].find("data-img").get("data-src")
-
-                    description = element.find(class_="truncate-text").text.strip().replace("\n", "").replace("\r", "")
-
-                    title_el = element.find(class_= "advertisement-item--content__title d-block text-truncate", href=True)
-                    link = title_el.get('href')
-                    title = title_el.text
+            title_el = element.find(class_= "advertisement-item--content__title d-block text-truncate", href=True)
+            link = title_el.get('href')
+            title = title_el.text
 
 
-                    stripped_prices = element.find(class_="advertisement-item--content__price").text.replace(" ", "").strip()
+            stripped_prices = element.find(class_="advertisement-item--content__price").text.replace(" ", "").strip()
 
-                    sq_m_el = element.find_all(class_="advertisement-item--content__info")
+            sq_m_el = element.find_all(class_="advertisement-item--content__info")
 
-                    for i in sq_m_el:
-                        i = i.text.strip()
-                        sq_m_index = i.find("•")
+            for i in sq_m_el:
+                i = i.text.strip()
+                sq_m_index = i.find("•")
 
-                        if sq_m_index != -1:
-                            sq_m = float(i[sq_m_index+2:-3].replace(",", "."))
-                        else:
-                            location_description = i
-
-
-                    # checks if the price is actually a number
-                    if re.match(r'\d+', stripped_prices[0]):
-                        index = stripped_prices.find("€")
-                        price = stripped_prices[:index]
-                        if price == '':
-                            price = None
-                        else:
-                            price = float(stripped_prices[:index].replace(",", "."))
-                    else:
-                        price = None
+                if sq_m_index != -1:
+                    sq_m = float(i[sq_m_index+2:-3].replace(",", "."))
+                else:
+                    location_description = i
 
 
-                    dict1["title"].append(title)
-                    dict1["location_description"].append(location_description)
-                    dict1["sq_m"].append(sq_m)
-                    dict1["price"].append(price)
-                    dict1["link"].append(link)
-                    dict1["img"].append(img)
-                    dict1["location"].append(cities[0]+1)
-                    if sale_rent_bool == 1 or sale_rent_bool == 2:
-                        dict1['buy'].append(1)
-                    else:
-                        dict1['buy'].append(0)
-                    if sale_rent_bool == 1 or sale_rent_bool == 3:
-                        dict1['house'].append(1)
-                    else:
-                        dict1['house'].append(0)
-
-                    # print(title)
-                    # print(location_description)
-                    # print(sq_m)
-                    # print(price)
-                    # print(link)
-                    # print(img)
-                    # print(description)
+            # checks if the price is actually a number
+            if re.match(r'\d+', stripped_prices[0]):
+                index = stripped_prices.find("€")
+                price = stripped_prices[:index]
+                if price == '':
+                    price = None
+                else:
+                    price = float(stripped_prices[:index].replace(",", "."))
+            else:
+                price = None
 
 
-                counter += 1
+            dict1["title"].append(title)
+            dict1["location_description"].append(location_description)
+            dict1["sq_m"].append(sq_m)
+            dict1["price"].append(price)
+            dict1["link"].append(link)
+            dict1["img"].append(img)
 
-                changing_url = f"https://www.nehnutelnosti.sk/{cities[1]}/{house_apartment_text}/{sale_rent_text}/?p[page]={counter}"
+            print(title)
+            print(location_description)
+            print(sq_m)
+            print(price)
+            print(link)
+            print(img)
+            # print(description)
 
-                html_doc = getHtmlDoc(changing_url)
-                soup = BeautifulSoup(html_doc, 'lxml')
-                logger.info(changing_url)
+
+
             
-            end=time.time()
-            logger.info(f"Scraping city {cities[1]} took {end-start} seconds")
+        end=time.time()
 
-            counter = 1
+        logger.info(f"Scraped page took {start-end}")
+
 
     main_end = time.time()
 
@@ -198,6 +154,7 @@ def scraping():
 
     return dict1
 
+scraping()
 
 def converting_to_df(ti):
     scraping_dict = ti.xcom_pull(task_ids="scraping")
@@ -211,16 +168,16 @@ def converting_to_df(ti):
     logger.info("Finished converting to dataframe")
 
 
-scraping = PythonOperator(
-    task_id="scraping",
-    python_callable=scraping,
-    dag=my_dag,
-)
+# scraping = PythonOperator(
+#     task_id="scraping",
+#     python_callable=scraping,
+#     dag=my_dag,
+# )
 
-converting_to_df = PythonOperator(
-    task_id="converting_to_df",
-    python_callable=converting_to_df,
-    dag=my_dag,
-)
+# converting_to_df = PythonOperator(
+#     task_id="converting_to_df",
+#     python_callable=converting_to_df,
+#     dag=my_dag,
+# )
 
-scraping >> converting_to_df
+# scraping >> converting_to_df
