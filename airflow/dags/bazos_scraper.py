35096a790a2b3524c3732c0bb60f9f83b3d77278
MrPotatorust <bumko96@gmail.com>
2024-10-27 23:58:21 +0100

    Bazos dag works and uploads to the database

--- airflow/dags/bazos_scraper.py
+++ airflow/dags/bazos_scraper.py
@@ -7,7 +7,9 @@ from bs4 import BeautifulSoup, NavigableString
 import requests
 from lxml import etree
 import re
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, Column, String, Integer, CHAR, Boolean, Float, DateTime, null
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 import pandas as pd
 import numpy as np
 import time
@@ -15,6 +17,58 @@ import json
 import logging
 
 from custom_functions import getHtmlDoc
+
+
+Base = declarative_base()
+
+class Advertisement(Base):
+    __tablename__ = "property_listings"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    
+    title = Column(String)
+    price = Column(Float, nullable=True)
+    sq_m = Column(Float, nullable=True)
+    img = Column(String)
+    link = Column(String)
+    location = Column(String)
+    postal_code = Column(String)
+    rentable = Column(Boolean, nullable=True)
+    property_type = Column(String, nullable=True)
+    site = Column(Integer)
+    datetime = Column(DateTime)
+
+    def __init__(self, title, price, sq_m, img, link, location, postal_code, 
+                rentable, property_type, site, datetime, id=None):
+        self.id = id
+        self.title = title
+        self.price = price
+        self.sq_m = sq_m
+        self.img = img
+        self.link = link
+        self.location = location
+        self.postal_code = postal_code
+        self.rentable = rentable
+        self.property_type = property_type
+        self.site = site
+        self.datetime = datetime
+
+    def __repr__(self):
+        return (f"Advertisement(id={self.id}, title='{self.title}', "
+                f"location='{self.location}', sq_m={self.sq_m}, "
+                f"price={self.price}, link='{self.link}', img='{self.img}', "
+                f"postal_code='{self.postal_code}', rentable={self.rentable}, "
+                f"property_type='{self.property_type}', site={self.site}, "
+                f"datetime='{self.datetime}')")
+
+
+conn_vars = BaseHook.get_connection('staging_db')
+engine = create_engine(f'postgresql://{conn_vars.login}:{conn_vars.password}@{conn_vars.host}:{conn_vars.port}/{conn_vars.schema}', echo=True)
+
+Base.metadata.create_all(bind=engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
 
 
 default_args = {
@@ -40,23 +94,27 @@ logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 logger = logging.getLogger(__name__)
 
 
-def scraping():
+def scraping(Advertisement = Advertisement, session = session):
 
     logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
     logger = logging.getLogger(__name__)
 
-    dict1 = {
-            "title": [],
-            "price": [],
-            "sq_m": [],
-            "img": [],
-            "link": [],
-            "location": [],
-            "postal_code":[],
-            'rentable': [],
-            'property_type': [],
-            'site':[]
-        }
+
+    cur_time = datetime.utcnow()
+
+    # dict1 = {
+    #         "title": [],
+    #         "price": [],
+    #         "sq_m": [],
+    #         "img": [],
+    #         "link": [],
+    #         "location": [],
+    #         "postal_code":[],
+    #         'rentable': [],
+    #         'property_type': [],
+    #         'site':[]
+    #     }
+
 
 
 
@@ -82,7 +140,7 @@ def scraping():
             if re.match(r'\d+', price[0]):
                 price = float(price)
             else:
-                price = None
+                price = null()
 
             
             element_loc = element.find(class_="inzeratylok").text.replace("\r", "").replace("\n", "")
@@ -93,16 +151,20 @@ def scraping():
 
             
             
-            dict1['title'].append(title)
-            dict1["location"].append(city)
-            dict1["sq_m"].append(None)
-            dict1['price'].append(price)
-            dict1['link'].append(f"https://reality.bazos.sk{link}")
-            dict1['img'].append(img)
-            dict1['postal_code'].append(postal_code)
-            dict1["rentable"].append(None)
-            dict1["property_type"].append(None)
-            dict1["site"].append(2)
+            # dict1['title'].append(title)
+            # dict1["location"].append(city)
+            # dict1["sq_m"].append(None)
+            # dict1['price'].append(price)
+            # dict1['link'].append(f"https://reality.bazos.sk{link}")
+            # dict1['img'].append(img)
+            # dict1['postal_code'].append(postal_code)
+            # dict1["rentable"].append(None)
+            # dict1["property_type"].append(None)
+            # dict1["site"].append(2)
+
+            advertisement = Advertisement(title=title, price=price, sq_m=null(), img=img, link=link, location=city, postal_code=postal_code, rentable=null(), property_type=null(), site=2, datetime=cur_time)
+
+            session.add(advertisement)
 
             # print(price)
             # print(description.replace("\r", "").replace("\n", ""))
@@ -119,32 +181,12 @@ def scraping():
         soup = BeautifulSoup(html_doc, "lxml")
         logger.info(f"scraped page {counter}")
 
-        break
 
     end = time.time()
 
     logger.info(f"Took: {end-start} seconds")
 
-    return dict1
-
-
-def converting_to_df(ti):
-    scraping_dict = ti.xcom_pull(task_ids="scraping")
-
-    logger.info("Converting to dataframe")
-
-    df = pd.DataFrame.from_dict(scraping_dict)
-    df.index.rename('id')
-
-    print(df.head())
-
-
-    logger.info("Finished converting to dataframe")
-
-    conn_vars = BaseHook.get_connection('staging_db')
-    engine = create_engine(f'postgresql://{conn_vars.login}:{conn_vars.password}@{conn_vars.host}:{conn_vars.port}/{conn_vars.schema}')
-
-    df.to_sql(name="property_listings", con=engine, if_exists='append')
+    session.commit()
 
 
 
@@ -154,10 +196,5 @@ scraping = PythonOperator(
     dag=my_dag,
 )
 
-converting_to_df = PythonOperator(
-    task_id="converting_to_df",
-    python_callable=converting_to_df,
-    dag=my_dag,
-)
 
-scraping >> converting_to_df
+scraping 
