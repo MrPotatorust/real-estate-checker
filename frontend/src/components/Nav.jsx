26898009ff021866fdff1aff9da674c357bfea97
MrPotatorust <bumko96@gmail.com>
2024-10-29 18:57:27 +0100

    Succesfully tested the nehnutelnosti script and started working on the frontend

---
+++ frontend/src/components/Nav.jsx
@@ -0,0 +1,10 @@
+export default function Nav(){
+    return (
+        <nav>
+            <ul class="nav--ul">
+                <li>First</li>
+                <li>Second</li>
+            </ul>
+        </nav>
+    )
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Root from "./routes/root.jsx";
+import ErrorPage from "./error-page.jsx";
+import HomePage from "./routes/HomePage.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./css/normalize.css";
+import "./css/index.css";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "",
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
